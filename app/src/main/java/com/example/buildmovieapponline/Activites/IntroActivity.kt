86999aafa79fffbb2848148d59b43da325309c4c
tungb <tungb100203@gmail.com>
2024-10-07 16:09:34 +0700

    Done Splash Screen

--- app/src/main/java/com/example/buildmovieapponline/Activites/IntroActivity.kt
+++ app/src/main/java/com/example/buildmovieapponline/Activites/IntroActivity.kt
@@ -3,12 +3,15 @@ package com.example.buildmovieapponline.Activites
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.buildmovieapponline.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1838)
+        installSplashScreen()
         binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
