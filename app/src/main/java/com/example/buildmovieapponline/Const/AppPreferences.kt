70d0e0e4d8dda74874048a26cde37ebca9705d56
tungb <tungb100203@gmail.com>
2024-11-05 08:33:18 +0700

    fix ui banner

--- app/src/main/java/com/example/buildmovieapponline/Const/AppPreferences.kt
+++ app/src/main/java/com/example/buildmovieapponline/Const/AppPreferences.kt
@@ -6,5 +6,10 @@ class AppPreferences {
         const val CHECK = "check"
         const val ISLOGGEDIN = "isLoggedIn"
         const val MAINACTIVITY = "MainActivity"
+        const val MOVIE_ID = "MOVIE_ID"
+        const val MOVIE_NAME = "MOVIE_NAME"
+        const val CATEGORY_ID = "CATEGORY_ID"
+        const val MOVIE_DURATION = "MOVIE_DURATION"
+        const val MOVIE_DESCRIPTION = "MOVIE_DESCRIPTION"
     }
 }
